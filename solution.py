500e8fbef012a3c1f0cd0dbd3441214a997c801b
SuperDorage <SuperDorage@gmail.com>
2019-01-02 20:45:53 +0900

    CHAPTER 1 EX

---
+++ solution.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+#################################
+# YOON's Python 3 - Fundamental #
+#################################
+
+print("CHAPTER 1 EXCERCISES : pg.20")
+
+def ex011_1():
+    print("Prints Name")
+
+def ex011_2():
+    print(1+2+3+4+5+6+7+8+9+10)
+
+def ex011_3():
+    print(2*2*2*2*2)
+
+def ex011_4():
+    print("5-(3-1) = ", 5-(3-1))
+
+def ex011_5():
+    print(10/2, 10*2)
+
+ex011_1()
+ex011_2()
+ex011_3()
+ex011_4()
+ex011_5()
+
