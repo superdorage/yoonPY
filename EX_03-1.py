135b3fa7d9e59fa4dceaf660217888694a71d87f
SuperDorage <SuperDorage@gmail.com>
2019-02-01 19:15:16 +0900

    [연습문제 03-1]

--- EX_03-1.py
+++ EX_03-1.py
@@ -1,35 +1,12 @@
 #!/usr/bin/env python3
 
-#############################
-# Jump to Python Excercises #
-#############################
+###################
+# [연습문제 03-1] #
+###################
 
+# input함수를 사용하여 '12' 와 '34' 를 차례로 입력 후 두 입력의 합 이 '1234' 로 출력 되는 지 확인하라.
 
-# Class
-
-class Calculator:
-    def __init__(self, numberList):
-        self.numberList = numberList
-        
-
-    #SUM, AVG
-    def sum(self):
-        self.x = 0
-        for i in self.numberList:
-            self.x += i
-        return self.x
-
-    def avg(self):
-        self.x = 0
-        for i in self.numberList:
-            self.x += i
-        return self.x / 5
-
-
-cal = Calculator([1,2,3,4,5])
-print(cal.sum())
-print(cal.avg())
-
-cal2 = Calculator([6,7,8,9,10])
-print(cal2.sum())
-print(cal2.avg())
+a = input("Enter first number: ")
+b = input("Enter second number: ")
+c = a + b
+print("Concatnates: ", c)
