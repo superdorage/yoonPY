f0779c082d7a9decd7a41f4654992e1fc6d1afad
SuperDorage <SuperDorage@gmail.com>
2019-02-10 16:03:03 +0900

    [연습문제 05-3]

---
+++ EX_05-3.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+###################
+# [연습문제 05-3] #
+###################
+
+
+"""
+>> str = "Hello"
+>> str = str + "Python"
+>> str
+
+실행결과:
+'HelloPython'
+
+"""
+
+# '+=' 연산자를 이용하여 위 코드를 바꾸어보자.
+
+str = 'Hello'
+str += 'Python'
+
+print(str)
+
+
